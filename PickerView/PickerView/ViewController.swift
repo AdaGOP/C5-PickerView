caa0f38bfe871b0af5ac0ffe9dde3b15ec7d7f10
zeinrezky <zeinrezky@gmail.com>
2022-06-07 15:14:34 +0700

    Customize picker with multiple dimention datasource

--- PickerView/PickerView/ViewController.swift
+++ PickerView/PickerView/ViewController.swift
@@ -11,15 +11,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var pickerView: UIPickerView!
     
-    // Preparing the dataSource
-    var pickerData: [String] = [String]()
+    // Preparing the dataSource with multiple dimention
+    var pickerData: [[String]] = [[String]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // Load the data source, this will be 1 dimention of data source
-        pickerData = ["Technical", "Design", "Product"]
+        pickerData = [["Technical", "Design", "Product"], ["morning", "afternoon"], ["cohort 1", "cohort 2", "cohort 3", "cohort 4", "cohort 5"]]
         
         // Connect pickerView to the datasource and delegate
         prepareConnection()
@@ -34,15 +34,19 @@ class ViewController: UIViewController {
 
 extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         pickerData.count
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        pickerData[row]
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        pickerData[component].count
     }
     
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        pickerData[component][row]
+    }
+    
+    // capturing what user selecting
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(pickerData[component][row])
+    }
 }
